2ac6ff4897d375d449161b15f6425bfeec28a730
ridho-dev <ridhopardede.dev@gmail.com>
2022-08-05 09:53:34 +0700

    Initial commit

---
+++ app/src/main/java/com/dededev/mysimplcleanarchitecture/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dededev.mysimplcleanarchitecture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
